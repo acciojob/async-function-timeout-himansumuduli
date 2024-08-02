182a525e75c850b5d42c246c245710154677c346
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-08-02 18:04:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async () => {
+    const text = document.getElementById('text').value;
+    const delay = parseInt(document.getElementById('delay').value);
+	await delayFunction(delay);
+	document.getElementById("output").innerText = text;
+})
+function delayFunction(delay) {
+	return new Promise((resolve) =>{
+		setTimeout(resolve,delay)
+	})
+	
+}
